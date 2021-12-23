5a5281fe7c8da2d15c82dddc8d1e4d0bd650a174
sans <1816074881@qq.com>
2021-12-22 17:23:33 -0800

    login and register

--- app.js
+++ app.js
@@ -3,39 +3,78 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var session = require('express-session');
+global.database = require('./database/mongo');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('html',require('ejs').__express);
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({ 
+	secret: 'secret',
+	cookie:{ 
+		maxAge: 1000*60*30
+	}
+}));
+app.use(function(req,res,next){ 
+	res.locals.user = req.session.user;
+	var err = req.session.error;
+	delete req.session.error;
+	res.locals.message = "";
+	if(err){ 
+		res.locals.message = '<div class="alert alert-danger" style="margin-bottom:20px;color:red;">'+err+'</div>';
+	}
+	next();
+});
 app.use('/', indexRouter);
+app.use('/register', indexRouter);
+app.use('/login', indexRouter);
+app.use('/home', indexRouter);
 app.use('/users', usersRouter);
+app.use('/logout', indexRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
+app.listen(10810)
 
 module.exports = app;
